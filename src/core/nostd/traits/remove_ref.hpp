2ae6dc1008437da0debe3b64980cf0e3a9bf1de3
Slava Aseev <sockskilling@gmail.com>
2024-01-25 18:42:13 +0300

    nostd/traits: Implement remove_rref

--- src/core/nostd/traits/remove_ref.hpp
+++ src/core/nostd/traits/remove_ref.hpp
@@ -16,10 +16,20 @@ namespace details_traits {
     struct remove_ref<T&&> {
         using type = T;
     };
+    template <typename T>
+    struct remove_rref {
+        using type = T;
+    };
+    template <typename T>
+    struct remove_rref<T&&> {
+        using type = T;
+    };
 } // namespace details_traits
 
 template <typename T>
 using remove_ref = typename details_traits::remove_ref<T>::type;
+template <typename T>
+using remove_rref = typename details_traits::remove_rref<T>::type;
 template <typename T>
 using remove_const_ref = typename details_traits::remove_const<typename details_traits::remove_ref<T>::type>::type;
 } // namespace core
