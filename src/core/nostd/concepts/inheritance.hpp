8335450a2bc92a9f0f16fcf7230f33fccdfed152
Slava Aseev <sockskilling@gmail.com>
2024-01-25 18:39:40 +0300

    nostd/concepts: Implement derived_from

--- src/core/nostd/concepts/inheritance.hpp
+++ src/core/nostd/concepts/inheritance.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "convertible_to.hpp"
+
 namespace core {
 template <typename T>
 concept empty_class = __is_empty(T);
@@ -9,4 +11,7 @@ concept final_class = __is_final(T);
 
 template <typename Base, typename Derived>
 concept base_of = bool(__is_base_of(Base, Derived));
+
+template <typename Derived, typename Base>
+concept derived_from = bool(__is_base_of(Base, Derived) && convertible_to<const volatile Derived*, const volatile Base*>);
 } // namespace core
