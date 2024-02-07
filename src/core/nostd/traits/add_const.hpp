f992e146ddb0ca13f5d0aaf4bc46e5f3d54ac387
Slava Aseev <sockskilling@gmail.com>
2024-02-07 17:56:43 +0300

    Implement some traits

---
+++ src/core/nostd/traits/add_const.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace core
+{
+template <typename T>
+using add_const = const T;
+}
---
+++ src/core/nostd/traits/add_cv.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace core
+{
+template <typename T>
+using add_cv = const volatile T;
+}
