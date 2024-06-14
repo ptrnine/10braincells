2da31718b6f7bf11d96ee52751162e773e86e1ff
Slava Aseev <sockskilling@gmail.com>
2024-06-14 23:21:45 +0300

    Fix core::conditional

--- src/core/traits/conditional.hpp
+++ src/core/traits/conditional.hpp
@@ -3,15 +3,15 @@
 namespace core {
 namespace details {
     template <bool, typename T1, typename>
-    struct conditional {
+    struct _conditional {
         using type = T1;
     };
     template <typename T1, typename T2>
-    struct conditional<false, T1, T2> {
+    struct _conditional<false, T1, T2> {
         using type = T2;
     };
 } // namespace details
 
 template <bool Cond, typename IfTrue, typename IfFalse>
-using conditional = typename details::conditional<Cond, IfTrue, IfFalse>::type;
+using conditional = typename details::_conditional<Cond, IfTrue, IfFalse>::type;
 } // namespace core
