c93032714bd7f6f3836d12ace00e9f5723e4e5de
Slava Aseev <sockskilling@gmail.com>
2024-11-09 04:30:30 +0300

    sys::open: Improve interface

--- src/sys/open.hpp
+++ src/sys/open.hpp
@@ -1,24 +1,40 @@
 #pragma once
 
+#include <core/concepts/string.hpp>
+
 #include <sys/open_flags.hpp>
 #include <sys/syscall.hpp>
 
+#define fwd(...) static_cast<decltype(__VA_ARGS__)>(__VA_ARGS__)
+
 namespace sys
 {
-inline auto openat(fd_t dirfd, const char* pathname, openflags oflags, file_perms mode = file_perms::none) {
-    oflags |= openflag::large;
-    return syscall<fd_t, trap_async_cancel>(SYS_openat, dirfd, pathname, oflags.value, mode.to_int());
+template <typename... Ts>
+    requires(core::null_term_string<core::remove_cvref<Ts>> || ... || false) &&
+            (core::convertible_to<Ts, openflags> || ... || false)
+inline auto openat(fd_t dirfd, Ts&&... args) {
+    const char* pathname;
+    openflags   flags = openflag::large;
+    file_perms  mode  = file_perms::none;
+
+    core::fwd_as_tuple(fwd(args)...)
+        .foreach (core::overloaded{
+            [&](const std::string& v) {
+                pathname = v.data();
+            },
+            [&](const char* v) {
+                pathname = v;
+            },
+            [&](openflags v) { flags |= v; },
+            [&](file_perms v) { mode |= v; },
+        });
+
+    return syscall<fd_t, trap_async_cancel>(SYS_openat, dirfd, pathname, flags.value, mode.to_int());
 }
 
-inline auto openat(fd_t dirfd, const std::string& pathname, openflags oflags, file_perms mode = file_perms::none) {
-    return openat(dirfd, pathname.data(), oflags, mode);
-}
-
-inline auto open(const char* pathname, openflags oflags, file_perms mode = file_perms::none) {
-    return openat(fdcwd, pathname, oflags, mode);
-}
-
-inline auto open(const std::string& pathname, openflags oflags, file_perms mode = file_perms::none) {
-    return open(pathname.data(), oflags, mode);
+inline auto open(auto&&... args) {
+    return openat(fdcwd, fwd(args)...);
 }
 } // namespace sys
+
+#undef fwd
