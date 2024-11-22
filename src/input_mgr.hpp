5b25549d75ff8d09d7181574bc70f5462f1da562
Slava Aseev <sockskilling@gmail.com>
2024-11-22 18:46:24 +0300

    Fix input_mgr
    - add checks for epoller
    - pass mouses to attached epoller

--- src/input_mgr.hpp
+++ src/input_mgr.hpp
@@ -19,6 +19,7 @@ public:
     }
 };
 
+/* TODO: build input device list */
 template <typename Epoller>
 class input_mgr {
 public:
@@ -27,10 +28,27 @@ public:
     void attach_to_epoller(Epoller& epoller) requires(!core::is_same<Epoller, void>) {
         _e = &epoller;
         _e->add_handler(core::int_c<event_group::input_device>, core::bind_member(*this, &input_mgr::epoller_handler));
+
+        for (sys::u32 slot = 0; slot < max_mouses; ++slot) {
+            auto mouse = _mouses[slot].load(std::memory_order_relaxed);
+            if (mouse && mouse->type() == inp::mouse_type::native)
+                add_dev_fd_to_epoller(mouse->as_native().fd(), slot);
+        }
     }
 
     void detach_epoller() {
-        _e->remove_handler(core::int_c<event_group::input_device>);
+        if (_e) {
+            _e->remove_handler(core::int_c<event_group::input_device>);
+
+            for (auto&& m : _mouses) {
+                if (auto mouse = m.load(std::memory_order_relaxed)) {
+                    if (mouse->type() == inp::mouse_type::native)
+                        _e->remove(mouse->as_native().fd());
+                }
+            }
+
+            _e = nullptr;
+        }
     }
 
     void attach_mouse(inp::generic_mouse& mouse) {
@@ -38,11 +56,10 @@ public:
 
         _mouses[slot].store(&mouse, std::memory_order_relaxed);
 
-        if (mouse.type() == inp::mouse_type::native)
-            _e->add(mouse.as_native().fd(),
-                    sys::epoll_event::in | sys::epoll_event::edge_triggered,
-                    event_group::input_device,
-                    sys::u32(slot));
+        if (_e && mouse.type() == inp::mouse_type::native) {
+            util::glog().detail("[input_mgr]: attach native mouse fd={} slot={}", int(mouse.as_native().fd()), slot);
+            add_dev_fd_to_epoller(mouse.as_native().fd(), sys::u32(slot));
+        }
 
         ++_mouses_count;
     }
@@ -52,7 +69,7 @@ public:
             if (m.load(std::memory_order_relaxed) == &mouse) {
                 m.store(nullptr, std::memory_order_release);
                 --_mouses_count;
-                if (mouse.type() == inp::mouse_type::native)
+                if (_e && mouse.type() == inp::mouse_type::native)
                     _e->remove(mouse.as_native().fd());
                 return;
             }
@@ -60,6 +77,10 @@ public:
     }
 
 private:
+    void add_dev_fd_to_epoller(sys::fd_t fd, sys::u32 slot) {
+        _e->add(fd, sys::epoll_event::in | sys::epoll_event::edge_triggered, event_group::input_device, slot);
+    }
+
     size_t find_free_mouse_slot() {
         for (size_t i = 0; i < max_mouses; ++i)
             if (!_mouses[i].load(std::memory_order_relaxed))
@@ -67,18 +88,17 @@ private:
         throw input_mgr_no_mouse_slot();
     }
 
-public:
     /* Can be called from different thread */
-    bool epoller_handler(sys::u32 dev_id, sys::fd_t, sys::epoll_events) {
+    core::epoller_handler_result epoller_handler(sys::u32 dev_id, sys::fd_t, sys::epoll_events) {
         if (dev_id >= max_mouses) {
             util::glog().error("input_mgr::epoller_handler: invalid device id {}", dev_id);
-            return false;
+            return core::epoller_handler_result::remove;
         }
 
         if (auto ptr = _mouses[dev_id].load(std::memory_order_acquire))
             ptr->as_native().handle();
 
-        return true;
+        return core::epoller_handler_result::keep_on;
     }
 
 private:
