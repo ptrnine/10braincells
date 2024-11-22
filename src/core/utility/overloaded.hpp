517be4169c559d4976303d48472e7ba5f7a0937c
Slava Aseev <sockskilling@gmail.com>
2024-11-22 18:39:10 +0300

    Add overloaded_default

--- src/core/utility/overloaded.hpp
+++ src/core/utility/overloaded.hpp
@@ -8,4 +8,12 @@ struct overloaded : Ts... {
 };
 template <class... Ts>
 overloaded(Ts...) -> overloaded<Ts...>;
+
+template <typename... Ts>
+static inline constexpr auto overload_default = overloaded<decltype([](Ts) {})...>{};
+
+template <typename T, typename... Ts>
+static inline constexpr auto overload_default_r = overloaded<decltype([](Ts) {
+    return T{};
+})...>{};
 } // namespace core
